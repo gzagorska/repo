466d75da3c285c06eb045993e957a5123269f3af
gzagorska <100200649+gzagorska@users.noreply.github.com>
2022-05-06 13:45:55 +0200

    Add files via upload

---
+++ Ciąg Fibonacciego/Ciąg Fibonacciego/Ciąg Fibonacciego.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	int f0 = 0;
+	int f1 = 1;
+	int f;
+
+	cout << "Podaj dlugosc ciagu Fibonacciego: ";
+	cin >> n;
+
+
+	for (int i = 0; i < n; i++)
+	{
+		if (i > 1)
+		{
+			f = f0 + f1;
+			f0 = f1;
+			f1 = f;
+		}
+		else
+		{
+			f = i;
+		}
+
+		cout << f << " ";
+
+	}
+
+
+}
